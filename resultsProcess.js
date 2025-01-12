6af74b75b784216e4e70e59e6843a040bf747ba4
sam5213 <gulmirazhb@gmail.com>
2025-01-12 21:58:51 +0500

    Update resultsProcess.js

--- resultsProcess.js
+++ resultsProcess.js
@@ -1,14 +1,3 @@
-export function showResults() {
-    const quizContainer = document.getElementById('quiz-container');
-    quizContainer.innerHTML = '<h2>Ваши результаты:</h2>';
-    results.forEach((result, index) => {
-        quizContainer.innerHTML += `<p>Вопрос ${index + 1}: ${result}</p>`;
-    });
-    document.getElementById('submit-results').style.display = 'block';
-    document.getElementById('share-container').style.display = 'block';
-    document.getElementById('share-vk').style.display = 'block'; // Показываем кнопку "Поделиться в VK"
-}
-
 export function selectAnswer(selected, correct) {
     userAnswers.push({ selected, correct }); // Сохраняем ответ пользователя
 }
